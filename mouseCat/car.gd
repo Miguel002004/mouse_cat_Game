da729016e8af8693a1190500c18a772277c63c06
José Miguel Reyes Márquez <miguel002004@hotmail.com>
2023-10-08 13:36:43 -0600

    ya salta

--- mouseCat/car.gd
+++ mouseCat/car.gd
@@ -1,32 +1,22 @@
 extends CharacterBody2D
 
-@export var speed = 400
-@export var jump_force = -500  # Ajusta según sea necesario
-var gravity = 10980  # Gravedad, ajusta según sea necesario
+@export var speed := 180
+var direccion := 0.0  # Ajusta según sea necesario
+var jump := 350  # Gravedad, ajusta según sea necesario
+const gravity := 10
 @onready var sprite = $Sprite2D
 @onready var viewport_rect = get_viewport().get_visible_rect()
 
 func _physics_process(delta):
-	velocity = Vector2.ZERO
-	
-	# Movimiento horizontal
+	direccion = Input.get_axis("ui_left", "ui_right")
+	velocity.x = direccion * speed
 	if Input.is_action_pressed('ui_left'):
-		velocity.x = -speed
 		sprite.flip_h = true
-	elif Input.is_action_pressed('ui_right'):
-		velocity.x = speed
+	if Input.is_action_pressed('ui_right'):
 		sprite.flip_h = false
-	
-	# Gravedad
-	velocity.y += gravity * delta
-
-	# Salto
-	if Input.is_action_just_pressed('ui_select') and is_on_floor():
-		velocity.y = jump_force
-	
-	# Límite de velocidad de caída
-	velocity.y = min(velocity.y, gravity)
-
+	if is_on_floor() and Input.is_action_just_pressed("ui_accept"):
+		velocity.y -= jump
+	velocity.y += gravity
 	# Movimiento y colisión
 	move_and_slide()  # Aquí está la corrección principal
 
